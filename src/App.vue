dc78bf4657c7aa4c3e98cdef9d94a0c352ab51c3
KAVIN SEBASTIAN <kavin.sebastian25@gmail.com>
2022-04-07 14:00:33 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> {{ 1 + 2 + 3 + 4 }} </h1>
+    <h1>with Methods {{ counter() }} </h1>
+    <h1>with Methods {{ counterParams(1, 3, 5) }} </h1>
+    <h1>multiple methods {{ multiply(baseValue) }} </h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:'app',
+  data(){
+    return{
+      baseMultiple: 5,
+      baseValue: 10
+    }
+  },
+  methods:{
+    counter(){
+      return 2 + 2 + 2 + 2
+    },
+    counterParams(a, b, c){
+      return a * b * c
+    },
+    multiply (num) {
+      return this.baseMultiple * num
+    }
   }
+
 }
 </script>
 
